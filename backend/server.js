9551430d486241ccccd53d75594496d6e58c7b35
GAGGAH1 <titusdcstd2022@gmail.com>
2026-08-24 12:06:28 +0100

    Included frontend url to my cors function and added the start with the node.js to the script in package.json

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [ process.env.FRONTEND_URL,'http://localhost:5173'];
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
